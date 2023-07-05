7e4d72b0746b3d72097c82030ea95d88b62da7d0
sungwoo <sungwoo7180@hanmail.net>
2023-07-05 23:39:46 +0900

    version1

---
+++ app/src/main/java/com/example/bottomnavi/Frag1.java
@@ -0,0 +1,30 @@
+package com.example.bottomnavi;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class
+Frag1 extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+
+
+        view = inflater.inflate(R.layout.frag1, container, false);
+        Log.e("","onCreateView");
+        return view;
+    }
+}

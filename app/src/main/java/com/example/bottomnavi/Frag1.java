b2dba3aa1a0d1e1106517f492ec329d6367921d0
sungwoo <sungwoo7180@hanmail.net>
2023-07-26 20:50:45 +0900

    version3

--- app/src/main/java/com/example/bottomnavi/Frag1.java
+++ app/src/main/java/com/example/bottomnavi/Frag1.java
@@ -21,16 +21,49 @@ public class Frag1 extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag1, container, false);
 
-        view.findViewById(R.id.my_button2).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.my_button2).setOnClickListener(new View.OnClickListener() {          //심장 대화상자
             @Override
             public void onClick(View v) {
                 showHeartDialog();
             }
         });
 
+        view.findViewById(R.id.my_button7).setOnClickListener(new View.OnClickListener() {          //저체온증 대화상자
+            @Override
+            public void onClick(View v) {
+                showHypothermiaDialog();
+            }
+        });
+
+        view.findViewById(R.id.my_button6).setOnClickListener(new View.OnClickListener() {          //열사병 대화상자
+            @Override
+            public void onClick(View v) {
+                showHeatstrokeDialog();
+            }
+        });
+
+        view.findViewById(R.id.my_button3).setOnClickListener(new View.OnClickListener() {          //상처 대화상자
+            @Override
+            public void onClick(View v) {
+               showWoundDialog();
+            }
+        });
+
+        view.findViewById(R.id.my_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 현재 프래그먼트를 새로운 프래그먼트(etc)로 교체합니다
+                Fragment etc = new ETC();
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+                transaction.replace(R.id.main_frame, etc);
+                transaction.addToBackStack(null); // 프래그먼트 트랜잭션을 백 스택에 추가
+                transaction.commit();
+            }
+        });
+
         return view;
     }
-
+    //심장 버튼 대화상자
     private void showHeartDialog() {
         final String[] heartOptions = {"응급처치 AED 사용법", "심폐소생술", "심장 질환 정보"};
 
@@ -48,6 +81,78 @@ public class Frag1 extends Fragment {
                         break;
                     case 2:
                         // Handle "심장 질환 정보" option
+                        break;
+                }
+            }
+        });
+        builder.setNegativeButton("취소", null);
+        builder.create().show();
+    }
+    //저체온증 버튼 대화상자
+    private void showHypothermiaDialog() {
+        final String[] heartOptions = {"저체온증", "동상"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("냉에 의한 손상");
+        builder.setItems(heartOptions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        navigateToFragment(new heartOption1());
+                        break;
+                    case 1:
+                        navigateToFragment(new heartOption2());
+                        break;
+                    case 2:
+                        break;
+                }
+            }
+        });
+        builder.setNegativeButton("취소", null);
+        builder.create().show();
+    }
+    //열사병 버튼 대화상자
+    private void showHeatstrokeDialog() {
+        final String[] heartOptions = {"열사병/일사병", "탈수", "열상화상", "화학화상"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("열에 의한 손상");
+        builder.setItems(heartOptions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        navigateToFragment(new heartOption1());
+                        break;
+                    case 1:
+                        navigateToFragment(new heartOption2());
+                        break;
+                    case 2:
+                        break;
+                }
+            }
+        });
+        builder.setNegativeButton("취소", null);
+        builder.create().show();
+    }
+    //상처 버튼 대화상자
+    private void showWoundDialog() {
+        final String[] heartOptions = {"상처의 종류", "상처 응급처치"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("상처");
+        builder.setItems(heartOptions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        navigateToFragment(new heartOption1());
+                        break;
+                    case 1:
+                        navigateToFragment(new heartOption2());
+                        break;
+                    case 2:
                         break;
                 }
             }
--- app/src/main/java/com/example/bottomnavi/MainActivity.java
+++ app/src/main/java/com/example/bottomnavi/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private FragmentTransaction ft;
     private Frag1 frag1;
     private Frag2 frag2;
-    private Frag3 frag3;
-    private Frag4 frag4;
+    private Frag4 frag3;
+    private Frag3 frag4;
 
 
     @Override
@@ -51,8 +51,8 @@ public class MainActivity extends AppCompatActivity {
         });
         frag1 = new Frag1();
         frag2 = new Frag2();
-        frag3 = new Frag3();
-        frag4 = new Frag4();
+        frag3 = new Frag4();
+        frag4 = new Frag3();
         setFrag(0);                 //첫 프레그먼트 화면을 무엇으로 지정해줄 것인지 선택.
     }
 
